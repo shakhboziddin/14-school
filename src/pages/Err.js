f96f6d56eee1be787a45312169c9e3d4ad29edb8
shahboziddin2007 <shahboziddin2007@gmail.com>
2024-05-15 17:00:40 +0500

    added fixed

--- src/pages/Err.js
+++ src/pages/Err.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import './err.css'
+import React from "react";
+import "./err.css";
 function Err() {
   return (
     <div>
-      <h1>Server didn't respond due to Payment errors</h1>
-      <p>Unfortunately, the server failed to respond to the request made, leaving users unable to access the desired information or service. This lack of response may be attributed to various factors, such as network congestion, server overload, or technical issues on the server side. The absence of a timely response disrupts the seamless flow of communication between client and server, resulting in inconvenience for users and potential disruptions to their tasks or activities. Troubleshooting efforts typically involve diagnosing the root cause of the server's unresponsiveness and implementing appropriate measures to restore functionality and prevent recurrence.</p>
+      <h1>fuck this school 💩💩💩💩💩</h1>
+      <p>all bitches they dont even pay for their website. 💩</p>
     </div>
-  )
+  );
 }
 
-export default Err
+export default Err;
